7e2520bc5c8ad9a54bedd64821dedf65e60a5cef
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-25 16:12:45 +0200

    Ajout appel API

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -7,10 +8,35 @@ import Posts from '../Posts/Posts';
 import './App.scss';
 
 import categoriesData from '../../data/categories';
-import postsData from '../../data/posts';
+import { PostInterface } from '../../@types';
+import Spinner from '../Spinner/Spinner';
 
 function App() {
   const [zenMode, setZenMode] = useState(false);
+
+  // Appel API
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [postsLoading, setPostsLoading] = useState(false);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        setPostsLoading(true);
+
+        const { data } = await axios.get(
+          `https://oblog-react.vercel.app/api/posts`
+        );
+
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setPostsLoading(false);
+      }
+    };
+
+    getPosts();
+  }, []);
 
   return (
     <div className={zenMode ? 'app app--zen' : 'app'}>
@@ -19,7 +45,7 @@ function App() {
         zenMode={zenMode}
         setZenMode={setZenMode}
       />
-      <Posts list={postsData} />
+      {postsLoading ? <Spinner /> : <Posts list={posts} />}
       <Footer />
     </div>
   );
