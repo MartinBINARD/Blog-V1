00dea72f90369851afed637d3d862ffbdb9de27c
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-28 10:12:10 +0200

    Ajout PromiseAll

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,66 +6,107 @@ import Header from '../Header/Header';
 import Posts from '../Posts/Posts';
 import Spinner from '../Spinner/Spinner';
 
-import './App.scss';
-
 import { CategoryInterface, PostInterface } from '../../@types';
+
+import './App.scss';
 
 function App() {
   const [zenMode, setZenMode] = useState(false);
 
-  // Appel API
-  const [categories, setCategories] = useState<CategoryInterface[]>([]);
+  // Appel API : articles
   const [posts, setPosts] = useState<PostInterface[]>([]);
-  const [postsLoading, setPostsLoading] = useState(false);
-  const [categoriesLoading, setcategoriesLoading] = useState(false);
+  // const [postsLoading, setPostsLoading] = useState(false);
+
+  // Appel API : catégories
+  const [categories, setCategories] = useState<CategoryInterface[]>([]);
+
+  // Appels API : loading commun
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getPosts = async () => {
+    async function fetchPosts(): Promise<PostInterface[]> {
       try {
-        setPostsLoading(true);
-
         const { data } = await axios.get(
-          `https://oblog-react.vercel.app/api/posts`
+          'https://oblog-react.vercel.app/api/posts'
         );
 
-        setPosts(data);
+        // on retourne
+        return data;
       } catch (error) {
-        console.log(error);
-      } finally {
-        setPostsLoading(false);
+        // eslint-disable-next-line no-console
+        console.error(error);
       }
-    };
 
-    const getCategories = async () => {
+      // si erreur, je retourne un tableau vide
+      return [];
+    }
+
+    async function fetchCategories(): Promise<CategoryInterface[]> {
       try {
-        setcategoriesLoading(true);
-
         const { data } = await axios.get(
-          `https://oblog-react.vercel.app/api/categories`
+          'https://oblog-react.vercel.app/api/categories'
         );
 
-        setCategories(data);
-      } catch (error) {
-        console.log('Error Categories: ', error);
-      } finally {
-        setcategoriesLoading(false);
-      }
-    };
+        // on stocke la réponse dans la variable d'état
+        // setCategories(data);
 
-    getPosts();
-    getCategories();
+        // je force un délai de 3 secondes pour stocker mes catégories
+        // pour simuler une différence de traitement entre les promesses
+        // return await new Promise((resolve) => {
+        //   setTimeout(() => {
+        //     resolve(data);
+        //   }, 3000);
+        // });
+        return data;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+
+      // si erreur, je retourne un tableau vide
+      return [];
+    }
+
+    /*
+      Ici on lance nos promesses les unes après les autres (sérialisation) ;
+      souvent on a besoin de résultats provenant de différentes promesses
+      en même temps
+      → c'est la parallélisation
+
+      (voir ./__docs/promise_all.png)
+    */
+    // fetchPosts();
+    // fetchCategories();
+    async function fetchData() {
+      const [categoriesData, postsData] = await Promise.all([
+        fetchCategories(),
+        fetchPosts(),
+      ]);
+
+      setCategories(categoriesData);
+      setPosts(postsData);
+      setIsLoading(false);
+    }
+
+    fetchData();
   }, []);
 
   return (
     <div className={zenMode ? 'app app--zen' : 'app'}>
-      {!postsLoading && !categoriesLoading && (
-        <Header
-          categories={categories}
-          zenMode={zenMode}
-          setZenMode={setZenMode}
-        />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Header
+            categories={categories}
+            zenMode={zenMode}
+            setZenMode={setZenMode}
+          />
+
+          {/* {postsLoading ? <Spinner /> : <Posts list={posts} />} */}
+          <Posts list={posts} />
+        </>
       )}
-      {postsLoading || categoriesLoading ? <Spinner /> : <Posts list={posts} />}
       <Footer />
     </div>
   );
